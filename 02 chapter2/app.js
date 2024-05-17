de402ce34d521c99cb8c42fcf697b48930ac88eb
yasmeenrahimhuda <155715580+yasmeenrahimhuda@users.noreply.github.com>
2024-05-17 10:00:12 +0500

    Add files via upload

---
+++ 02 chapter2/app.js
@@ -0,0 +1,70 @@
+// Q1. Declare a variable called username.
+
+var username ="Yasmeen";
+
+alert(username)
+
+// Q2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+
+var myname = "Yasmeen Rahim";
+
+alert(myname)
+
+//  Q3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+var titled ="Hello World";
+
+alert(titled)
+
+// Q4 Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+// student's bio data
+
+var firstName = prompt("Enter your first name:");
+var lastName = prompt("Enter your last name:");
+var age = prompt("Enter your age:");
+var grade = prompt("Enter your grade:");
+var school = prompt("Enter your school:");
+
+alert("Student Bio Data:\n\n" +
+      "First Name: " + firstName + "\n" +
+      "Last Name: " + lastName + "\n" +
+      "Age: " + age + "\n" +
+      "Grade: " + grade + "\n" +
+      "School: " + school);
+
+    // Q5  Write a script to display the following alert using one JS
+    //   variable:
+      var helloMessage = "Hello,\n\nWelcome to our website!";
+     
+      alert(helloMessage);
+
+    //Q6  Declare a variable called email and assign to it a string that
+    // represents your Email Address(e.g. example@example.com).
+    // Show the blow mentioned message in an alert box.(Hint: use
+    // string concatenation)
+
+    var email = "abc@gmail.com";
+    var alertMessage = "My email address is " + email;
+    alert(alertMessage);
+
+    // Q7 Declare a variable called book & give it the value “A
+    // smarter way to learn JavaScript”. Display the following
+    // message in an alert box:
+    
+    var book = "A smarter way to learn JavaScript";
+    alert("I am currently reading the book: '" + book + "'");
+
+    // Q8 Write a script to display this in browser through JS
+
+document.write("<p>Yah! I can write HTML content through JavaScript</p>");
+
+    // Q9. Store following string in a variable and show in alert and
+    // browser through JS
+
+    var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+    alert(design);
